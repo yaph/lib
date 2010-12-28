3845cefb44f7f16a9ac06e3b04daefc3e68f9236
Ramiro Gómez <web@ramiro.org>
2010-12-28 23:23:40 +0100

    added leadingZero function and calls to it

--- javascript/countdown.js
+++ javascript/countdown.js
@@ -21,10 +21,18 @@ function countdown(y, m, d, h, mi, s, ms) {
     var dmin = Math.floor(mhour / min);
     var dsec = Math.floor((mhour % min) / sec);
     day_text = dday == 1 ? 'day' : 'days';
-    var html = '<div id="cd"><div id="cdday">'+ dday +' '+ day_text +'</div><div id="cdtime">'+dhour+':'+dmin+':'+dsec+'</div></div>';
+    var html = '<div id="cd"><div id="cdday">'+ dday +' ' + day_text +'</div><div id="cdtime">'
+      +leadingZero(dhour)+':'+leadingZero(dmin)+':'+leadingZero(dsec) +'</div></div>';
     document.getElementById('countdown').innerHTML=html;
     setTimeout(function(){countdown(y, m, d, h, mi, s, ms);},1000);
   } else {
     document.getElementById('countdown').innerHTML='<div id="cd"><h3 id="cdh3">Euro 2012 started</h3></div>';
   }
+}
+function leadingZero(num) {
+  str = '' + num;
+  if (str.length < 2) {
+    str = 0 + str;
+  }
+  return str;
 }
