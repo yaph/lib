13b539c979667a815d79f68f31c7ee3d53a457af
Ramiro Gomez <web@ramiro.org>
2011-01-26 01:08:15 +0100

    - added HTTP class for performing HTTP requests and setting the HTTP status code for later retrieval - added documentation

--- php/wcc.php
+++ php/wcc.php
@@ -4,6 +4,12 @@
  */
 class WCC {
   /**
+   * Set to the full URL when the request method is called.
+   * @var string
+   */
+  protected $url = '';
+
+  /**
    * File system root path to cache dir.
    * @var string
    */
@@ -20,65 +26,163 @@ class WCC {
    * @var object
    */
   protected $cache = NULL;
-  
+
+  /**
+   * Creates a WCC object
+   * @param array $opts Associative options array
+   * @return void
+   */
   public function __construct($opts) {
     $this->cache_lifetime = $opts['cache_lifetime'];
     $this->fs_cache_root_path = $opts['fs_cache_root_path'];
     $this->cache = new FSCache($this->fs_cache_root_path);
-    #$this->cache = new FSCache();
+    $this->http = new HTTP();
   }
 
+  /**
+   * Requests data from Web or tries to load from cache if $cache_lifetime is an integer value
+   * @param string $url Full URL or base URL when params is set to add a query string
+   * @param array $params Associative array of URL parameters and values
+   * @param int | false $cache_lifetime Cache lifetime in seconds
+   * @return mixed $response Data loaded from cache or Web
+   */
   public function request($url, $params = array(), $cache_lifetime = false) {
+    $this->url = $this->getRequestURL($url, $params);
+    // determine wether to try to load data from cache
     $get_from_cache = $response = false;
-    $url = $this->getRequestURL($url, $params);
     if (false === $cache_lifetime)
       $cache_lifetime = $this->cache_lifetime;
     if (false !== $cache_lifetime)
       $get_from_cache = true;
-
+    // try to load from cache
     if ($get_from_cache) {
-      $id = $this->cache->getIDFromURL($url);
+      $id = $this->cache->getIDFromURL($this->url);
       $response = $this->cache->get($id, $cache_lifetime);
     }
-
-    if (!$get_from_cache || !$response)
-      $response = file_get_contents($url);
-
+    // load from Web
+    if (!$get_from_cache || !$response) {
+      $response = $this->http->get($this->url);
+    }
+    // cache data if data exists and caching is requested
     if ($response && $get_from_cache) {
       if (!isset($id))
-        $id = $this->cache->getIDFromURL($url);
+        $id = $this->cache->getIDFromURL($this->url);
       $this->cache->set($id, $response);
     }
-
     return $response;
   }
 
-  public function getRequestURL($url, $params = array()) {
+  /**
+   * Returns full URL requested
+   * @param void
+   * @return string $url
+   */
+  public function getURL() {
+    return $this->url;
+  }
+
+  /**
+   * Returns full URL to request, params are added when set
+   * @param string $url Full URL or base URL when params is set to add a query string
+   * @param array $params Associative array of URL parameters and values
+   * @return string $url
+   */
+  private function getRequestURL($url, $params = array()) {
     if ($params) {
       asort($params);
       $url .= '?' . http_build_query($params, '', '&');
     }
     return $url;
   }
+
+  /**
+   * Returns HTTP status code returned after Web request
+   * @param void
+   * @return int $status
+   */
+  public function getHTTPStatus() {
+    return $this->http->getStatus();
+  }
 }
 
+/**
+ * A class for performing HTTP requests
+ */
+class HTTP {
+  /**
+   * HTTP status code received after request
+   * @var int $status
+   */
+  private $status;
+
+  /**
+   * Performs a get HTTP request and returns the content received or false
+   * @param string $url
+   * @return string $body
+   */
+  public function get($url) {
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $body = curl_exec($ch);
+    $this->status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if (200 > $this->status || 300 <= $this->status)
+      return false;
+    return $body;
+  }
+
+  /**
+   * Returns the HTTP status code from request
+   * @param string $url
+   * @return int $this->status
+   */
+  public function getStatus() {
+    return $this->status;
+  }
+}
+
+/**
+ * URLCache interface definition
+ */
 interface URLCache {
   public function get($id, $lifetime);
   public function set($id, $data);
   public function getIDFromURL($url);
 }
 
+/**
+ * File system cache class the implements the URLCache interface
+ */
 class FSCache implements URLCache {
+  /**
+   * UMASK setting for writing and change permissions operations
+   * @var int
+   */
   const UMASK = 0777;
+
+  /**
+   * Full path to file system cache dir
+   * @var string
+   */
 
   private $root_dir = '/tmp';
 
+  /**
+   * Create cache object
+   * @param string $root_dir Full file system path of cache dir
+   * @return void
+   */
   public function __construct($root_dir = '') {
     if ($root_dir)
       $this->root_dir = $root_dir;
   }
+
   /**
-   * @param $id file name
+   * Get cached data if exists otherwise returns false
+   * @param string $id File name
+   * @param int $lifetime Cache lifetime
+   * @return mixed Cached data or false
    */
   public function get($id, $lifetime) {
     if (file_exists($id)) {
@@ -89,8 +193,10 @@ class FSCache implements URLCache {
   }
 
   /**
+   * Write cache data to file system and create directories as needed
    * @param int   $id file name
    * @param mixed $data
+   * @return bool $succes
    */
   public function set($id, $data) {
     $success = false;
@@ -108,6 +214,12 @@ class FSCache implements URLCache {
     return $success;
   }
 
+  /**
+   * Returns the cache ID created from the URL, which is an absolute file name
+   * with URL host name as main directory and URL path as subdirectories.
+   * @param string $url
+   * @return string $id
+   */
   public function getIDFromURL($url) {
     $dir = $this->root_dir;
     $parts = parse_url($url);
